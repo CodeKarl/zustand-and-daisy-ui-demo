6b28e38ea90a0d0c04d6969cec99eb046af97d41
Karel Reitsma <reitsma.karel@gmail.com>
2024-02-04 21:53:33 +0000

    Add more research

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -10,6 +10,7 @@ export const useStore = create<CounterSlice & DataFetchSlice>((...a) => ({
   // That's what we call namespacing slices, which is more advanced.
   // Some third party libraries support namespacing. For example: zustand-lens in https://github.com/pmndrs/zustand/blob/3c03e40f93e20eb28ab50fc84a7e64af92bf63c5/docs/integrations/third-party-libraries.md
   // https://github.com/dhmk083/dhmk-zustand-lens
+  // https://github.com/pmndrs/zustand/issues/178 This is a issue that discusses namespacing
   ...createCounterSlice(...a),
   ...createDataFetchSlice(...a),
 }))
