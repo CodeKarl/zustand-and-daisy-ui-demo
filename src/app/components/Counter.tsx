8c29d118938cc8e4694b6f0641ca9137afd3826c
Karel Reitsma <reitsma.karel@gmail.com>
2024-02-04 17:43:51 +0000

    Setup simple example for daisyui

--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -9,14 +9,20 @@ function Counter() {
   const decreaseCount = useStore((state: any) => state.decreaseCount)
   const resetCount = useStore((state: any) => state.resetCount)
 
+  const hasDisabledReset = currentCount === 0
+
   return (
-    <div>
-      <h1>Counter</h1>
-      <p>Current count: {currentCount}</p>
+    <div className='card relative flex flex-col gap-4 justify-center shadow-xl bg-base-100 border border-white px-16 pt-4 pb-16 rounded-lg'>
+      <h1 className='text-center'>Counter</h1>
+      <p className='text-center'>{currentCount}</p>
       <div className='flex gap-2'>
-        <button type="button" onClick={increaseCount}>+</button>
-        <button type="button" onClick={resetCount}>Reset</button>
-        <button type="button" onClick={decreaseCount}>-</button>
+        <button type="button" className='btn btn-circle btn-error' onClick={decreaseCount}>-</button>
+        <button type="button" className='btn btn-warning' disabled={hasDisabledReset} onClick={resetCount}>Reset</button>
+        <button type="button" className='btn btn-circle btn-success' onClick={increaseCount}>+</button>
+      </div>
+      <div className='absolute bottom-0 right-0 flex gap-2 p-2'>
+        <div className="badge badge-primary">Zustand</div>
+        <div className="badge badge-secondary">DaisyUI</div>
       </div>
     </div>
   )
